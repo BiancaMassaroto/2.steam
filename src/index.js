d206e8e8ab9f3aba90d5d195cd6337d73923ff58
Bianca <bianca.s.massaroto@gmail.com>
2024-09-10 16:01:46 -0300

    Add new features to API

--- src/index.js
+++ src/index.js
@@ -52,6 +52,42 @@ steam.post("/tag", async (req, res) => {
 	}
 })
 
+steam.put("/update/:id", async (req, res) => {
+	try {
+		const id = req.params.id
+		const {gameTitle} = req.body
+		let update = await connection.connection.promise().query(`
+			UPDATE games
+			SET title = (?)
+			WHERE games.id = (?)	
+		`, [gameTitle, id])
+		res.status(200).send(update)
+	} catch (err) {
+		console.log(err)
+	}
+})
+
+steam.delete("/delete/:id", async (req, res) => {
+	try{
+		const idJogo = req.params.id
+		let deleteGame = await connection.connection.promise().query(`DELETE FROM games WHERE id = (?)`, [idJogo])
+		res.status(200).send(deleteGame) 
+	} catch (err) {
+		console.log(err)
+	}
+})
+
+steam.delete("/delete/tag/:id", async (req, res) => {
+	try{
+		const idTag = req.params.id
+		let deleteTag = await connection.connection.promise().query(`DELETE FROM tag WHERE id = (?)`, [idTag])
+		res.status(200).send(deleteTag) 
+	} catch (err) {
+		console.log(err)
+	}
+})
+
+
 steam.listen(3000, () => {
 	console.log("Steam 2.0 rodando na porta 3000.");
 });
